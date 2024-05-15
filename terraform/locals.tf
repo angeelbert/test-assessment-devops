b5f0310a7e5476571d79eca39d9651045e18d5a5
angeelbert <angeelbert@hotmail.com>
2024-05-15 16:38:11 -0400

    ecs module

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,4 +3,16 @@ locals {
   table_name  = "csgtest"
 
   ecr_repo_csgtest = "csgtest-app-ecr-repo"
+
+  demo_app_cluster_name        = "csgtest-app-cluster"
+  availability_zones           = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  demo_app_task_famliy         = "csgtest-app-task"
+  container_port               = 3000
+  demo_app_task_name           = "csgtest-app-task"
+  ecs_task_execution_role_name = "csgtest-app-task-execution-role"
+
+  application_load_balancer_name = "csgtest-app-alb"
+  target_group_name              = "csgtest-alb-tg"
+
+  demo_app_service_name = "csgtest-app-service"
 }
